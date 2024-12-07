af97afef874d5539c6c2f0c2fcd902a9b8f1aaab
Rashid Isaev <isarutul@yandex.ru>
2024-12-08 00:01:07 +0300

    Replace the popup with a modal window. Implement all the elements on the window

--- 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/MainWindow.xaml.cs
+++ 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/MainWindow.xaml.cs
@@ -25,18 +25,18 @@ namespace DownloadManager
 
         private async void DownloadPages(object sender, RoutedEventArgs e)
         {
-            string[] addresses = txtBox.Text.Split(',');
-            cts = new CancellationTokenSource();
-            CancellationToken token = cts.Token;
-           // var t = Task.Run(async()=> await Downloader.Download(addresses, token));
-            statBarText.Text="Downloading...";
-            downloadBtn.IsEnabled = false;
-            cancelBtn.IsEnabled = true;
+            //string[] addresses =  addWindowTextBox.Text.Split(',');
+            //cts = new CancellationTokenSource();
+            //CancellationToken token = cts.Token;
+            //var t = Task.Run(async()=> await Downloader.Download(addresses, token));
+            //statBarText.Text="Downloading...";
+            //downloadBtn.IsEnabled = false;
+            //cancelBtn.IsEnabled = true;
             //await t;
-            await Downloader.Download(addresses, token);
-            cancelBtn.IsEnabled = false;
-            downloadBtn.IsEnabled = true;
-            statBarText.Text = "Ready";
+            //await Downloader.Download(addresses, token);
+            //cancelBtn.IsEnabled = false;
+            //downloadBtn.IsEnabled = true;
+            //statBarText.Text = "Ready";
         }
 
         private void CancelDownloading(object sender, RoutedEventArgs e)
@@ -53,9 +53,21 @@ namespace DownloadManager
 
         }
 
-        private void discardUrls(object sender, RoutedEventArgs e)
+        private void DiscardUrls(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void EnterValue(object sender, KeyEventArgs e)
         {
 
         }
+
+        private void AddUrlBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddUrlWindow auw = new();
+            auw.ShowDialog();
+        }
+
+ 
     }
 }

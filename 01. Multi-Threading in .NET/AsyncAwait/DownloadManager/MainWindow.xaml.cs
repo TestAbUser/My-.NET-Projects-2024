59c8dc9f0e6467c0a40109cacbbf7a580196f153
Rashid Isaev <isarutul@yandex.ru>
2024-12-10 12:37:50 +0300

    Clean up MainWindow code-behind file

--- 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/MainWindow.xaml.cs
+++ 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/MainWindow.xaml.cs
@@ -19,43 +19,9 @@ namespace DownloadManager
     public partial class MainWindow : Window
     {
         public MainWindowViewModel ViewModel { get; set; } = new MainWindowViewModel();
-      //  CancellationTokenSource cts;
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        //private async void DownloadPages(object sender, RoutedEventArgs e)
-        //{
-        //    string[] addresses = addWindowTextBox.Text.Split(','); 
-        //    cts = new CancellationTokenSource();
-        //    CancellationToken token = cts.Token;
-        //    var t = Task.Run(async () => await Downloader.Download(addresses, token));
-        //    statBarText.Text = "Downloading...";
-        //    downloadBtn.IsEnabled = false;
-        //    cancelBtn.IsEnabled = true;
-        //    await t;
-        //    await Downloader.Download(addresses, token);
-        //    cancelBtn.IsEnabled = false;
-        //    downloadBtn.IsEnabled = true;
-        //    statBarText.Text = "Ready";
-        //}
-
-        //private void CancelDownloading(object sender, RoutedEventArgs e)
-        //{
-        //    cts.Cancel();
-        //    cts.Dispose();
-        //    cancelBtn.IsEnabled = false;
-        //    downloadBtn.IsEnabled = true;
-        //    statBarText.Text="Ready";
-        //}
-
-
-
-        //private void AddUrlBtn_Click(object sender, RoutedEventArgs e)
-        //{
-        //    AddUrlWindow auw = new();
-        //    auw.ShowDialog();
-        //}
     }
 }

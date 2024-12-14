11eb6bf95b90f8b3f3c3a207f35062282d7fa9dd
Rashid Isaev <none>
2024-12-14 13:11:06 +0300

    Remove the unnecessary code related to UrlModel class from AddUrlWindowViewModel

--- 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/ViewModels/AddUrlWindowViewModel.cs
+++ 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/ViewModels/AddUrlWindowViewModel.cs
@@ -1,37 +1,28 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
 using DownloadManager.Commands;
-using DownloadManager.Models;
 
 namespace DownloadManager.ViewModels
 {
     public class AddUrlWindowViewModel
     {
-        private readonly UrlModel _urlModel;
         private RelayCommand<object> _okCommand;
 
         // Url property is bound to the Text property of the TextBox.
         public string Url { get; set; }
 
-        // public ObservableCollection<UrlModel> UModels { get; } = new ObservableCollection<UrlModel>();
         public ObservableCollection<string> Urls { get; } = new();
         public AddUrlWindowViewModel()
         {
         }
         public AddUrlWindowViewModel(ObservableCollection<string> Urls)
         {
-            // _urlModel = url;
-            _urlModel = new UrlModel();
             this.Urls = Urls;
-           // UModels = UrlModels;
         }
 
         // When OK button is clicked addUrlWindow is passed as a CommandParameter to this property.
         public RelayCommand<object> OkCommand => _okCommand ??= new RelayCommand<object>(obj =>
         {
-            //_urlModel.Urls.Add(Url);
-            // _urlModel.Url = Url;
-            // UModels.Add(_urlModel);
             Urls.Add(Url);
             // Casting the argument to Window. 
             Window wnd = obj as Window;

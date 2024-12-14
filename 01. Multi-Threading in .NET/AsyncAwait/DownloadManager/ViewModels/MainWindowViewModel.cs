c8693c886408fa001170f9fc85124e8a94f77f17
Rashid Isaev <none>
2024-12-14 13:06:27 +0300

    Remove the unnecessary code related to UrlModel class from MainWindowViewModel

--- 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/ViewModels/MainWindowViewModel.cs
+++ 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/ViewModels/MainWindowViewModel.cs
@@ -1,46 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using System.Windows;
-using DownloadManager.Models;
+﻿using System.Windows;
 using DownloadManager.Commands;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
-using System.Windows.Documents;
 
 namespace DownloadManager.ViewModels
 {
-    public class MainWindowViewModel: INotifyPropertyChanged
+    public class MainWindowViewModel
     {
-        private ObservableCollection<string> _urls;
         CancellationTokenSource cts;
-
-        private readonly UrlModel _urlModel = new UrlModel();
-        public UrlModel UModel { get; set; }// = new UrlModel();
 
         private RelayCommand _openAddWindowCommand = null;
 
-       // public ObservableCollection<UrlModel> UrlModels { get; } = new ObservableCollection<UrlModel>();
-        public ObservableCollection<string> Urls { get; }= new();
-        //{
-        //    get { return _urls; }
-        //    set
-        //    {
-        //        _urls = value;
-        //        PropertyChanged(this, new PropertyChangedEventArgs(nameof(Urls)));
-        //    }
-        //}
-        public MainWindowViewModel()
-        {
-           // UModel.Url = "test";
-            //Urls= _urlModel.Urls;
-           //UrlModels = UModel.Urls;
-        }
-
+        public ObservableCollection<string> Urls { get; } = new();
 
         public RelayCommand OpenAddWindowCommand
         {
@@ -48,20 +18,12 @@ namespace DownloadManager.ViewModels
             {
                 return _openAddWindowCommand ??= new RelayCommand(() =>
                 {
-                    //UModel = new UrlModel();
-                   // UrlModels.Add(UModel);
-                    AddUrlWindowViewModel viewModel = new (Urls/*UModel*//*_urlModel*/);
+                    // Pass the collection holding Urls to the other window's view model.
+                    AddUrlWindowViewModel viewModel = new(Urls);
                     AddUrlWindow auw = new(viewModel);
                     auw.ShowDialog();
                 });
             }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged(string prop = "")
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
 
         private async void DownloadPages(object sender, RoutedEventArgs e)

756d34b13c1838cc9efdeec7d0e3b4c59eebbfc8
Rashid Isaev <isarutul@yandex.ru>
2024-12-20 21:22:38 +0300

    Remove unnecessary usings from MainWindow

--- 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/MainWindow.xaml.cs
+++ 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/MainWindow.xaml.cs
@@ -1,14 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using DownloadManager;
+﻿using System.Windows;
 using DownloadManager.ViewModels;
 
 namespace DownloadManager

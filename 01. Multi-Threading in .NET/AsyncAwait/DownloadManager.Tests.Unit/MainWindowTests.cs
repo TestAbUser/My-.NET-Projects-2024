3ba0e1f4adb6e9466b6e6be666f63784d636a249
Rashid Isaev <isarutul@yandex.ru>
2025-02-02 01:22:03 +0300

    Add tests for MainWindow

---
+++ 01. Multi-Threading in .NET/AsyncAwait/DownloadManager.Tests.Unit/MainWindowTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DownloadManager.Helpers;
+using DownloadManager.ViewModels;
+using Moq;
+using Xunit;
+
+namespace DownloadManager.Tests.Unit
+{
+    public class MainWindowTests
+    {
+        [Fact]
+        public void Load_urls_from_file()
+        {
+            var fileSystemMock = new Mock<IFileSystem>();
+            fileSystemMock.Setup(x => x.LoadFileContent())
+                .Returns(["testUrl1", "testUrl2"]);
+            var sut = new MainWindowViewModel(fileSystemMock.Object);
+
+            sut.OpenCommand.Execute(null);
+            // sut.SaveCommand.Execute(null);
+
+            fileSystemMock.Verify(
+                x => x.SaveDialog(new string[] { "testUrl1", "testUrl2" }),
+                Times.Once);
+        }
+
+        [Fact]
+        public void Save_urls_to_a_file()
+        {
+            
+        }
+    }
+}

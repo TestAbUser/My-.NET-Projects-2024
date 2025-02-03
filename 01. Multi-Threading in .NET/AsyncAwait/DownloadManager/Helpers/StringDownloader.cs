c750d2e873f0eed331735c17ababe0749e83f7d4
Rashid Isaev <isarutul@yandex.ru>
2025-02-04 02:00:12 +0300

    Move the external dependency on a web-page

---
+++ 01. Multi-Threading in .NET/AsyncAwait/DownloadManager/Helpers/StringDownloader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadManager.Helpers
+{
+    internal class StringDownloader: IStringDownloader
+    {
+        private static readonly HttpClient s_client = new();
+
+        public async Task<string> DownloadPageAsStringAsync(string url, CancellationToken ct)
+        {
+           return await s_client.GetStringAsync(url, ct);
+        }
+    }
+}
